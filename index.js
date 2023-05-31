c50eec6c758d00393b3585dbfb73503923783a83
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-05-31 12:44:00 +0000

    Read the Query String

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Hello from CodeSandbox!"); //write a response to the client
+    res.write(req.url);
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
