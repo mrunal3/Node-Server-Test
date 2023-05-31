42825d300874f2c5fcf52edde7862c3b3ded70d9
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-05-31 12:48:18 +0000

    Split the Query String

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 /* https://www.w3schools.com/nodejs/nodejs_http.asp */ 
 
 var http = require("http");
+var url = require('url');
 
 //create a server object:
 http
   .createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
-    res.end(); //end the response
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
   })
   .listen(8080); //the server object listens on port 8080
+
+/*
+  The address:
+  http://localhost:8080/?year=2017&month=July
+*/ 
