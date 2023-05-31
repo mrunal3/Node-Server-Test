af6adecb9ec62439cdf6deaddfcd7946fffc95ec
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-05-31 12:32:50 +0000

    Add an HTTP Header

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+/* https://www.w3schools.com/nodejs/nodejs_http.asp */ 
+
 var http = require("http");
 
 //create a server object:
 http
   .createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("Hello from CodeSandbox!"); //write a response to the client
     res.end(); //end the response
   })
