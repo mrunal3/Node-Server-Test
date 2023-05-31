8dfb7f639d98406a16cdcf0207c2aa8c5be21f41
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-05-31 13:08:39 +0000

    Node.js as a File Server: Create Files

--- demo_readfile.js
+++ demo_readfile.js
@@ -1,9 +1,9 @@
+/* https://www.w3schools.com/nodejs/nodejs_filesystem.asp */ 
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+    fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 }).listen(8080);
