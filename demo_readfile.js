384659e72a187f787a3f8ceeb0eaafe66b8b9521
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-05-31 13:24:23 +0000

    The fs.unlink() method deletes the specified file

--- demo_readfile.js
+++ demo_readfile.js
@@ -22,4 +22,8 @@ http.createServer(function (req, res) {
         if (err) throw err;
         console.log('Replaced!');
     });
+    fs.unlink('mynewfile2.txt', function (err) {
+        if (err) throw err;
+        console.log('File deleted!');
+    });
 }).listen(8080);
