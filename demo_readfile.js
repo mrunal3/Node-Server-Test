4f1f1e301499e0c8cb6bed9afc3d18e1475f9c8a
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-05-31 13:22:32 +0000

    Replace the content of the file "mynewfile3.txt"

--- demo_readfile.js
+++ demo_readfile.js
@@ -18,4 +18,8 @@ http.createServer(function (req, res) {
         if (err) throw err;
         console.log('Updated!');
     });
+    fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+        if (err) throw err;
+        console.log('Replaced!');
+    });
 }).listen(8080);
