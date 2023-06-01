8c3da17199f398bcd967dd39c09b098f29e15f0c
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-06-01 08:19:42 +0000

    Create a Node.js file that writes an HTML form, with an upload field

---
+++ nodejs_uploadfiles/nodejs_uploadfiles.js
@@ -0,0 +1,13 @@
+/* https://www.w3schools.com/nodejs/nodejs_uploadfiles.asp */ 
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+  res.write('<input type="file" name="filetoupload"><br>');
+  res.write('<input type="submit">');
+  res.write('</form>');
+  return res.end();
+  
+}).listen(8080); //the server object listens on port 8080
