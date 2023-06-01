19231952bfdcdd83928d93aa46baeffd5ebc36f9
Mrunal Alatkar <52127474+mrunal3@users.noreply.github.com>
2023-06-01 10:05:01 +0000

    Save the File

--- nodejs_uploadfiles/nodejs_uploadfiles.js
+++ nodejs_uploadfiles/nodejs_uploadfiles.js
@@ -2,13 +2,42 @@
 
 var http = require('http');
 var formidable = require('formidable');
+var fs = require('fs');
+const path = require('path');
 
 http.createServer(function (req, res) {
   if (req.url == '/fileupload') {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      res.write('File uploaded');
-      res.end();
+      var oldpath = files.filetoupload.filepath;
+      var relativePath = './' + files.filetoupload.originalFilename;
+      const newpath = path.resolve(__dirname, relativePath);
+      
+      // console.log('File Path:', newpath);
+      // fs.rename(oldpath, newpath, function (err) {
+      //   if (err) throw err;
+      //   res.write('File uploaded and moved!');
+      //   res.end();
+      // });
+
+      fs.readFile(oldpath, function (err, data) {
+        if (err) throw err;
+        console.log('File read!');
+
+        // Write the file
+        fs.writeFile(newpath, data, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+            console.log('File written!');
+        });
+
+        // Delete the file
+        fs.unlink(oldpath, function (err) {
+            if (err) throw err;
+            console.log('File deleted!');
+        });
+      });
     });
   } else {
     res.writeHead(200, {'Content-Type': 'text/html'});
